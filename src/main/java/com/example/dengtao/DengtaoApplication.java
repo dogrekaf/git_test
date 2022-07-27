6071c7a91aaee7b38a288240cd704f8719f58c4a
LAPTOP-NKTRT05D\dengt <dengtao1994416@qq.com>
2022-07-27 10:06:40 +0800

    IDEA整合github测试

---
+++ src/main/java/com/example/dengtao/DengtaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dengtao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DengtaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DengtaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dengtao/DengtaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dengtao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DengtaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
